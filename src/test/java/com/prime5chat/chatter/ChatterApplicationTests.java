ef47b4445f3c6c56d7bc456b8d2a1c8f03801393
SandyPham823 <sandydupham@gmail.com>
2019-12-09 15:03:38 -0500

    first commit

---
+++ src/test/java/com/prime5chat/chatter/ChatterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prime5chat.chatter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
