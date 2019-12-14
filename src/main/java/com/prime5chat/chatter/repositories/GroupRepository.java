3ae4a83e636d0882de7bc6459f3a33438c742d51
SandyPham823 <sandydupham@gmail.com>
2019-12-13 19:58:34 -0500

    gimme dev

--- src/main/java/com/prime5chat/chatter/repositories/GroupRepository.java
+++ src/main/java/com/prime5chat/chatter/repositories/GroupRepository.java
@@ -2,6 +2,8 @@ package com.prime5chat.chatter.repositories;
 
 import com.prime5chat.chatter.models.Group;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface GroupRepository extends CrudRepository<Group, Long> {
 }
