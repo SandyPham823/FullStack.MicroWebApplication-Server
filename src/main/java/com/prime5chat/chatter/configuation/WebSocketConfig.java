cbfe9d55afb692814a5c2a04822f08d116e5d1a1
MikeGray31 <michaelkgray31@gmail.com>
2019-12-16 09:21:45 -0500

    update

--- src/main/java/com/prime5chat/chatter/configuation/WebSocketConfig.java
+++ src/main/java/com/prime5chat/chatter/configuation/WebSocketConfig.java
@@ -12,7 +12,9 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/prime5chatter").setAllowedOrigins("*").withSockJS();
+        registry.addEndpoint("/prime5chatter")
+                .setAllowedOrigins("*")
+                .withSockJS();
     }
 
     @Override
